5f385e548619298f2a92aefa4e73edfa2e680c00
Mohd Tanveer Hasan <mtanveerhasan2001@gmail.com>
2023-03-04 15:22:15 +0530

    initiate

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import MovieInfo from './movie-info.js'
+function App() {
+  return (
+    <>
+      <MovieInfo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/movie-info.js
@@ -0,0 +1,13 @@
+import CastCard from './components/char-cast-card.js';
+export default function MovieInfo() {
+    return (
+        <>
+            <CastCard
+                character="Mitsuha Miyamizu"
+                role="Lead"
+                engvoice="Stephanie Sheh"
+                japvoice="Mone Kamishiraishi"
+            />
+        </>
+    );
+}
